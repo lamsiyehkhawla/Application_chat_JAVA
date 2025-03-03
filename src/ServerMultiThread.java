a796d93c109c4654d54110be848ee7f11832f7dc
khawla lamsiyeh <61538329+khawlalam@users.noreply.github.com>
2025-03-03 13:16:06 +0100

    add comments

--- src/ServerMultiThread.java
+++ src/ServerMultiThread.java
@@ -7,18 +7,30 @@ import java.util.Random;
 
 public class ServerMultiThread {
     public static void main(String[] args) throws IOException {
+        // Create a server socket that listens on port 9090
         ServerSocket serverSocket = new ServerSocket(9090);
+
+        // Generate a random number between 0 and 99 (the "magic number")
         Random random = new Random();
         int nbMagique = random.nextInt(100);
-        List<Socket> sockets = new ArrayList<Socket>();
+
+        // List to store all connected clients
+        List<Socket> sockets = new ArrayList<>();
+
+        System.out.println("Server is running on port 9090...");
+
+        // Infinite loop to accept multiple client connections
         while (true) {
+            // Accept an incoming client connection
             Socket socket = serverSocket.accept();
+            System.out.println("New client connected: " + socket.getInetAddress());
+
+            // Add the new client's socket to the list
             sockets.add(socket);
-            SocketThread socketThread = new SocketThread(socket, nbMagique,sockets);
+
+            // Start a new thread to handle this client
+            SocketThread socketThread = new SocketThread(socket, nbMagique, sockets);
             socketThread.start();
         }
-
-
     }
-
 }
--- src/SocketThread.java
+++ src/SocketThread.java
@@ -4,19 +4,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SocketThread extends Thread {
-    private Socket socket;
-    private int nbMagic;
-    private String joueurName;
-    private List<Socket> sockets;
-    public SocketThread(Socket socket, int nbMagic, List<Socket> sockets) {
+    private Socket socket; // Client socket
+    private int nbMagic; // The magic number to guess
+    private String joueurName; // Player's name
+    private List<Socket> sockets; // List of all connected clients
 
+    // Constructor to initialize the client socket, magic number, and client list
+    public SocketThread(Socket socket, int nbMagic, List<Socket> sockets) {
         this.socket = socket;
         this.nbMagic = nbMagic;
         this.sockets = sockets;
     }
+
+    // Method to notify all other players when someone finds the magic number
     private void broadcastMessage() throws IOException {
         for (Socket s : sockets) {
-            if (s != socket) { // Send message to all other players
+            if (s != socket) { // Send message to all other players except the current one
                 OutputStream os = s.getOutputStream();
                 PrintWriter ps = new PrintWriter(new OutputStreamWriter(os), true);
                 ps.println("Le joueur " + joueurName + " a trouvé le nombre magique : " + nbMagic);
@@ -27,6 +30,7 @@ public class SocketThread extends Thread {
     @Override
     public void run() {
         try {
+            // Setup input and output streams for communication
             InputStream inputStream = socket.getInputStream();
             OutputStream outputStream = socket.getOutputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
@@ -34,31 +38,33 @@ public class SocketThread extends Thread {
             Scanner sc = new Scanner(System.in);
             int nbJoueur;
 
+            // Send welcome message to the player
             ps.println("Bienvenue dans 'GUESS THE MAGIC NUMBER' !!!");
-            ps.println("Please enter your name : !!!");
+
+            // Ask the player for their name
+            ps.println("Please enter your name : ");
             joueurName = br.readLine();
-            ps.println("Enter a number : !!!");
 
+            // Ask the player to start guessing the number
+            ps.println("Enter a number: ");
 
+            // Game loop - keep asking until the player finds the correct number
             do {
-                nbJoueur = Integer.parseInt(br.readLine());
-                if (nbJoueur > nbMagic) {
-                    ps.println("Entrez un nombre inférieur.");
-                }else if (nbJoueur < nbMagic) {
-                    ps.println("Entrez un nombre supérieur.");
-                }else {
-                    ps.println("Bravo! Vous avez trouvé: " + nbMagic);
-                    broadcastMessage();
+                nbJoueur = Integer.parseInt(br.readLine()); // Read player's guess
 
+                if (nbJoueur > nbMagic) {
+                    ps.println("Entrez un nombre inférieur."); // Hint to enter a smaller number
+                } else if (nbJoueur < nbMagic) {
+                    ps.println("Entrez un nombre supérieur."); // Hint to enter a larger number
+                } else {
+                    ps.println("Bravo! Vous avez trouvé: " + nbMagic); // Player guessed correctly
+                    broadcastMessage(); // Notify other players
                 }
 
-            }while(nbJoueur !=nbMagic);
-
-
+            } while (nbJoueur != nbMagic); // Repeat until the correct number is guessed
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
